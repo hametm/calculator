167b8194dfecd203108b0f69dbccfd14c6e7d6ea
Morgan Hamet <morganhamet@gmail.com>
2022-06-07 00:23:41 -0700

    Make calculator functions work for multiple operators in a row

--- scripts.js
+++ scripts.js
@@ -12,26 +12,31 @@ let operatorClicked = false;
 let equalsClicked = false;
 let tempFirstNumber = "";
 let tempSecondNumber = "";
+let answer = "";
 
 
 function add(num1, num2) {
     console.log(num1 + num2);
-    display.textContent = num1 + num2;
+    answer = num1 + num2;
 }
 
 function subtract(num1, num2) {
     console.log(num1 - num2);
-    display.textContent = num1 - num2;
+    answer = num1 - num2;
 }
 
 function multiply(num1, num2) {
     console.log(num1 * num2);
-    display.textContent = num1 * num2;
+    answer = num1 * num2;
 }
 
 function divide(num1, num2) {
+    if (num2 == 0) {
+        display.textContent = "ERROR DIVIDE BY ZERO";
+        return 0;
+    }
     console.log(num1 / num2);
-    display.textContent = num1 / num2;
+    answer = num1 / num2;
 }
 
 function operate(operator, num1, num2) {
@@ -49,7 +54,7 @@ function operate(operator, num1, num2) {
             divide(num1, num2);
             break;
         default:
-            return "Error";
+            return "ERROR";
     }
 }
 
@@ -61,10 +66,14 @@ function reset() {
     secondClickedNumber = "";
     clickedOperator = "";
     operatorClicked = false;
+    equalsClicked = false;
 }
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
+        if (equalsClicked) {
+            reset();
+        }
         display.textContent += String(button.id);
     });
 });
@@ -83,15 +92,26 @@ numbers.forEach(button => {
 
 operators.forEach(button => {
     button.addEventListener('click', () => {
-        operatorClicked = !(operatorClicked);
-        clickedOperator = button.id;
+        if (operatorClicked == true && equalsClicked == false) {
+            clickedOperator = button.id;
+            operate(clickedOperator, +firstClickedNumber, +secondClickedNumber);
+            display.textContent = answer + button.id;
+            firstClickedNumber = answer;
+            tempSecondNumber = "";
+            secondClickedNumber = "";
+        } else {
+            clickedOperator = button.id;
+            operatorClicked = !(operatorClicked);
+        }
     });
 });
 
 equals.addEventListener('click', () => {
+    equalsClicked = true;
     console.log("First number: " + firstClickedNumber);
     console.log("Second number: " + secondClickedNumber);
     operate(clickedOperator, +firstClickedNumber, +secondClickedNumber);
+    display.textContent = answer;
 });
 
 clear.addEventListener('click', () => {
